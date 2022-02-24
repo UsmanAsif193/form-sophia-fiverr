f895669963c757159f24278523dd0c033869ddf2
Haseeb Asif <36411814+HaseebAsif@users.noreply.github.com>
2022-02-24 18:21:45 +0500

    design working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import "./index.css";
 
 const App = () => {
-  return <div>App</div>;
+  const [hover, setHover] = useState(false);
+  const HandleForm = () => {};
+
+  return (
+    <div
+      className={`image-container ${hover && "image-container-hover"}`}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      <img
+        className={`image-1 ${hover && "image-1-hover"}`}
+        src="logo1.svg"
+        alt=""
+      />
+      <img
+        className={`image-2 ${hover && "image-2-hover"}`}
+        src="logo2.svg"
+        alt=""
+      />
+
+      {/* Form Starts from Here */}
+      {hover && (
+        <form action="" className="form">
+          <textarea
+            className="text-area"
+            id="form"
+            name="form"
+            rows="20"
+            cols="50"
+          >
+            At w3schools.com you will learn how to make a website.
+          </textarea>
+          <button>Send Message</button>
+        </form>
+      )}
+    </div>
+  );
 };
 
 export default App;
